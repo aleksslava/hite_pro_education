9bb9854c00af516cf66ad1b65c84271018a91d29
Vuacheslav <soloomonslava1991@gmail.com>
2026-02-12 11:35:52 +0300

    Скрыта отправка видео с вебинарами

--- amo_api/amo_api.py
+++ amo_api/amo_api.py
@@ -682,6 +682,20 @@ class AmoCRMWrapper:
 
 
 
+if __name__ == '__main__':
+    from config.config import load_config, Config
+    config: Config = load_config()
 
+    amo_api = AmoCRMWrapper(
+        path=config.amo_config.path_to_env,
+        amocrm_subdomain=config.amo_config.amocrm_subdomain,
+        amocrm_client_id=config.amo_config.amocrm_client_id,
+        amocrm_redirect_url=config.amo_config.amocrm_redirect_url,
+        amocrm_client_secret=config.amo_config.amocrm_client_secret,
+        amocrm_secret_code=config.amo_config.amocrm_secret_code,
+        amocrm_access_token=config.amo_config.amocrm_access_token,
+        amocrm_refresh_token=config.amo_config.amocrm_refresh_token
+    )
+    amo_api.init_oauth2()
 
 
