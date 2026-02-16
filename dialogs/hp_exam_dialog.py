55a0171e1b28b8eb7f84e70e5570aecdf01e0217
Vuacheslav <soloomonslava1991@gmail.com>
2026-02-16 16:02:32 +0300

    Скрыта отправка видео с вебинарами

--- dialogs/hp_exam_dialog.py
+++ dialogs/hp_exam_dialog.py
@@ -238,6 +238,8 @@ result = Window(
     Format(text="<b>Экзамен не пройден!</b>🥹\n\n", when='result_text'),
     Format(text="{result_text}", when='result_text'),
     Format(text="{compleat_text}", when='passed'),
+    Format(text='Смотрите <a href="https://vk.com/video-140176277_456239582?list=ln-ZzlVOBtZszjuNCd61Z&clckid=1d2b9df5">видеопоздравление</a> от основателя компании Анатолия Кайибханова!',
+           when='passed'),
     Column(
         Url(Const('🔵 Сообщить в Telegram'), url=Format("{url_tg}"), when='passed'),
         Url(Const('🟢 Сообщить в WhatsApp'), url=Format("{url_wa}"), when='passed'),
